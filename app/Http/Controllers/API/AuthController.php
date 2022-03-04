991abcb83c80793f75f761161873fd0dcd944a5e
Veljko <veljkos82@gmail.com>
2022-03-04 18:40:38 +0100

    Refactored some code.

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -48,7 +48,7 @@ class AuthController extends Controller
         $accessToken = auth()->user()->createToken("authToken")->accessToken;
         $user = User::with("role", "vehicles.type")->find(auth()->user()->id);
     
-        return response(["Email is sent." , $user, "access_token" => $accessToken]);
+        return response(["Email is sent." , "user" => $user , "access_token" => $accessToken]);
 
     }
 
